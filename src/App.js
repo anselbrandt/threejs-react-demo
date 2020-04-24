c63c12578f50d847ae855a15ef80cb8823044809
anselbrandt <ansel@anselbrandt.com>
2020-04-23 20:57:02 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./App.module.css";
+import ThreeCanvas from "./ThreeCanvas";
+
+function App() {
+  return (
+    <div className={styles.app}>
+      <ThreeCanvas />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ThreeCanvas.js
@@ -0,0 +1,10 @@
+import React from "react";
+import * as THREE from "three";
+
+export default function ThreeCanvas() {
+  return (
+    <canvas
+      style={{ width: "800px", height: "800px", backgroundColor: "whitesmoke" }}
+    ></canvas>
+  );
+}
