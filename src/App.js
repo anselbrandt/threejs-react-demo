466073c9c987d70c404dd8006a62ddd4b772c5f8
anselbrandt <ansel@anselbrandt.com>
2020-04-24 03:53:50 -0400

    Add .gltf model

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./App.module.css";
+import useGetViewport from "./useGetViewport";
 import ThreeCanvas from "./ThreeCanvas";
 
 function App() {
+  const { width } = useGetViewport();
+  const canvasRef = useRef();
   return (
     <div className={styles.app}>
-      <ThreeCanvas />
+      <ThreeCanvas canvasRef={canvasRef} width={width / 2} height={width / 2} />
     </div>
   );
 }
--- src/ThreeCanvas.js
+++ src/ThreeCanvas.js
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { RGBELoader } from "three/examples/jsm/loaders/RGBELoader";
+import { RoughnessMipmapper } from "three/examples/jsm/utils/RoughnessMipmapper";
 
-export default function ThreeCanvas() {
-  return (
-    <canvas
-      style={{ width: "800px", height: "800px", backgroundColor: "whitesmoke" }}
-    ></canvas>
-  );
+export default function ThreeCanvas(props) {
+  const { canvasRef, width, height } = props;
+
+  useEffect(() => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+      45,
+      window.innerWidth / window.innerHeight,
+      0.25,
+      20
+    );
+    camera.position.set(-1.8, 0.6, 2.7);
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    const render = () => renderer.render(scene, camera);
+    const pmremGenerator = new THREE.PMREMGenerator(renderer);
+    pmremGenerator.compileEquirectangularShader();
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    new RGBELoader()
+      .setDataType(THREE.UnsignedByteType)
+      .setPath("./equirectangular/")
+      .load("quarry_01_1k.hdr", function (texture) {
+        const envMap = pmremGenerator.fromEquirectangular(texture).texture;
+        scene.background = envMap;
+        scene.environment = envMap;
+        texture.dispose();
+        pmremGenerator.dispose();
+        render();
+        const roughnessMipmapper = new RoughnessMipmapper(renderer);
+        const loader = new GLTFLoader().setPath("./DamagedHelmet/glTF/");
+        loader.load("plane.gltf", function (gltf) {
+          gltf.scene.traverse(function (child) {
+            if (child.isMesh) {
+              roughnessMipmapper.generateMipmaps(child.material);
+            }
+          });
+          scene.add(gltf.scene);
+          roughnessMipmapper.dispose();
+          render();
+        });
+      });
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.toneMapping = THREE.ACESFilmicToneMapping;
+    renderer.toneMappingExposure = 0.8;
+    renderer.outputEncoding = THREE.sRGBEncoding;
+
+    canvasRef.current.appendChild(renderer.domElement);
+    render();
+    controls.addEventListener("change", render); // use if there is no animation loop
+    controls.minDistance = 2;
+    controls.maxDistance = 10;
+    controls.target.set(0, 0, -0.2);
+    controls.update();
+  }, [canvasRef, width, height]);
+
+  return <div ref={canvasRef}></div>;
 }
