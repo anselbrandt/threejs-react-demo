72a55634f849b657d69d49e596a80cd7fe7b9376
anselbrandt <ansel@anselbrandt.com>
2020-04-28 08:47:29 -0400

    Remove helpers

--- src/ThreeCanvas.js
+++ src/ThreeCanvas.js
@@ -44,9 +44,6 @@ export default function ThreeCanvas(props) {
     hemiLight.position.set(0, 50, 0);
     scene.add(hemiLight);
 
-    // const hemiLightHelper = new THREE.HemisphereLightHelper(hemiLight, 10);
-    // scene.add(hemiLightHelper);
-
     const dirLight = new THREE.DirectionalLight(0xffffff, 1);
     dirLight.color.setHSL(
       colors.dirlight.h,
@@ -66,9 +63,6 @@ export default function ThreeCanvas(props) {
     dirLight.shadow.camera.far = 3500;
     dirLight.shadow.bias = -0.0001;
     scene.add(dirLight);
-
-    // const dirLightHeper = new THREE.DirectionalLightHelper(dirLight, 10);
-    // scene.add(dirLightHeper);
 
     // Ground
 
@@ -129,11 +123,6 @@ export default function ThreeCanvas(props) {
       model.rotation.y = rotation.yaw;
       model.rotation.z = rotation.roll;
     }
-
-    // Axes
-
-    // const axesHelper = new THREE.AxesHelper(25);
-    // scene.add(axesHelper);
 
     // Renderer
 
